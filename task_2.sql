bf6ca88c7896be0fa45f29dba5d4c21281664e82
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-10-19 21:22:19 +0000

    2. Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -4,7 +4,7 @@ USE alx_book_store;
 -- ==========================
 -- 1. AUTHORS TABLE
 -- ==========================
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
@@ -12,13 +12,13 @@ CREATE TABLE IF NOT EXISTS authors (
 -- ==========================
 -- 2. BOOKS TABLE
 -- ==========================
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS books (
 -- ==========================
 -- 3. CUSTOMERS TABLE
 -- ==========================
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
@@ -36,11 +36,11 @@ CREATE TABLE IF NOT EXISTS customers (
 -- ==========================
 -- 4. ORDERS TABLE
 -- ==========================
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
@@ -48,15 +48,15 @@ CREATE TABLE IF NOT EXISTS orders (
 -- ==========================
 -- 5. ORDER_DETAILS TABLE
 -- ==========================
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
